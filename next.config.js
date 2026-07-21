0651147345c5c71be144b6dbe0f5cd5fd1007572
popera454-wq <popera454@gmail.com>
2026-07-21 23:19:46 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // מתעלם משגיאות ניסוח/גרשיים בזמן ה-Build ב-Vercel
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    // מתעלם מאזהרות טיפוסים קטנות בזמן ה-Build
-    ignoreBuildErrors: true,
-  },
-};
-
-module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   eslint: { ignoreDuringBuilds: true },
   typescript: { ignoreBuildErrors: true },
 };
